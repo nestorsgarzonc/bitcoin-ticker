b948926643465cf2ff5e60e5a1d7326b2e5270ab
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-18 10:40:44 -0500

    build for specific platform

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -36,37 +38,45 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-              height: 150.0,
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(bottom: 10.0),
-              color: Colors.lightBlue,
-              child: CupertinoPicker(
-                itemExtent: 32,
-                backgroundColor: Colors.lightBlue,
-                onSelectedItemChanged: (selectedIndex) {
-                  print(selectedIndex);
-                },
-                children: currenciesList.map<Text>((value) {
-                  return Text(value, style: TextStyle(color: Colors.white));
-                }).toList(),
-              )),
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 10.0),
+            color: Colors.lightBlue,
+            child:Platform.isAndroid?getDropDownButton():getCupertinoPicker(),
+          ),
         ],
       ),
     );
+  } 
+
+  DropdownButton<String> getDropDownButton() {
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        icon: Icon(Icons.arrow_downward),
+        iconSize: 24,
+        onChanged: (String value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        },
+        items: currenciesList.map<DropdownMenuItem<String>>((value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList());
+  }
+
+  CupertinoPicker getCupertinoPicker() {
+    return CupertinoPicker(
+      itemExtent: 32,
+      backgroundColor: Colors.lightBlue,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: currenciesList.map<Text>((value) {
+        return Text(value, style: TextStyle(color: Colors.white));
+      }).toList(),
+    );
   }
 }
-/* DropdownButton<String>(
-                value: selectedCurrency,
-                icon: Icon(Icons.arrow_downward),
-                iconSize: 24,
-                onChanged: (String value) {
-                  setState(() {
-                    selectedCurrency = value;
-                  });
-                },
-                items: currenciesList.map<DropdownMenuItem<String>>((value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList()), */
