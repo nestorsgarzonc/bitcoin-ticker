b77c258902956cf025fb1c047b890dbfc86ca77d
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-19 08:45:19 -0500

    progresss

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +10,45 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = currenciesList[0];
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
+
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await CoinData().getCoinData(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Column makeCards() {
+    List<CryptoCard> cryptoCards = [];
+    for (String crypto in cryptoList) {
+      cryptoCards.add(
+        CryptoCard(
+          criptoCurrency: crypto,
+          selectedCurrency: selectedCurrency,
+          value: isWaiting ? '?' : coinValues[crypto],
+        ),
+      );
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: cryptoCards,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,42 +60,28 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Text(
-                '1 BTC = ? USD',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-          ),
+          makeCards(),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 10.0),
             color: Colors.lightBlue,
-            child:Platform.isAndroid?getDropDownButton():getCupertinoPicker(),
+            child:
+                Platform.isAndroid ? getDropDownButton() : getCupertinoPicker(),
           ),
         ],
       ),
     );
-  } 
+  }
 
   DropdownButton<String> getDropDownButton() {
     return DropdownButton<String>(
         value: selectedCurrency,
         icon: Icon(Icons.arrow_downward),
         iconSize: 24,
-        onChanged: (String value) {
-          setState(() {
-            selectedCurrency = value;
-          });
+        onChanged: (String value) async {
+          selectedCurrency = value;
+          getData();
         },
         items: currenciesList.map<DropdownMenuItem<String>>((value) {
           return DropdownMenuItem<String>(
@@ -77,6 +101,31 @@ class _PriceScreenState extends State<PriceScreen> {
       children: currenciesList.map<Text>((value) {
         return Text(value, style: TextStyle(color: Colors.white));
       }).toList(),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  final String value;
+  final String selectedCurrency;
+  final String criptoCurrency;
+  CryptoCard({this.value, this.selectedCurrency, this.criptoCurrency});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text(
+          '1 $criptoCurrency = $value $selectedCurrency',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
     );
   }
 }
